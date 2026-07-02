76e63c909a055bc1a62f2045bcf107404797e437
HaowenRong <Alexrong1116@gmail.com>
2026-07-02 18:07:21 +0100

    added public security group, and public and private instances

--- main.tf
+++ main.tf
@@ -94,6 +94,30 @@ resource "aws_nat_gateway" "ngw" {
 }
 
 # security groups
+resource "aws_security_group" "ssh_public_sg" {
+  name        = "ssh-public-sg"
+  description = "Allow SSH from the public subnet"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.public_subnet_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-public-sg"
+  }
+}
+
 resource "aws_security_group" "ssh_private_sg" {
   name        = "ssh-private-sg"
   description = "Allow SSH from only the private subnet"
@@ -115,5 +139,43 @@ resource "aws_security_group" "ssh_private_sg" {
 
   tags = {
     Name = "ssh-private-sg"
+  }
+}
+
+# get aws ami information
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# instances
+resource "aws_instance" "public" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ssh_public_sg.id.id]
+  key_name               = var.key_name
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "public-instance"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.ssh_private_sg.id.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "private-instance"
   }
 }
