335d65dc7d7e91c50c38d264c3e0567db2a1ea00
HaowenRong <Alexrong1116@gmail.com>
2026-07-05 15:14:14 +0100

    fixed typo in security group ids

--- main.tf
+++ main.tf
@@ -158,7 +158,7 @@ resource "aws_instance" "public" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.ssh_public_sg.id.id]
+  vpc_security_group_ids = [aws_security_group.ssh_public_sg.id]
   key_name               = var.key_name
 
   associate_public_ip_address = true
@@ -172,7 +172,7 @@ resource "aws_instance" "private" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.private.id
-  vpc_security_group_ids = [aws_security_group.ssh_private_sg.id.id]
+  vpc_security_group_ids = [aws_security_group.ssh_private_sg.id]
   key_name               = var.key_name
 
   tags = {
